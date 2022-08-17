82e4be67b318d8fc4419fe095d2838551dbe4fdf
Bhavyarajdev <109733933+Bhavyarajdev@users.noreply.github.com>
2022-08-17 23:16:26 +0530

    Add files via upload

---
+++ Jquery/demo1.js
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+ <script>
+	 function demoexternalalert()
+	 {
+		 alert("external alert!!!!")
+	 }
+	 function demoexternalconfirm()
+	 {
+		 if(confirm("are you sure????"))
+			 {
+				 alert("yesss");
+			 }
+		 else
+			 {
+				 alert("nooo");
+			 }
+	 }
+	 function demoexternalprompt()
+	 {
+		 var name=prompt("enter your name");
+		 alert(name);
+	 }
+</script>
+</head>
+
+<body>
+</body>
+</html>
